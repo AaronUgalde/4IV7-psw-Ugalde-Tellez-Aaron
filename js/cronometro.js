8f1d06507061f21d599c92baa8192da898fd588e
LicAaron <aaronugaldet@gmail.com>
2021-04-12 17:48:00 -0500

    src y js cronometro

--- js/cronometro.js
+++ js/cronometro.js
@@ -10,6 +10,8 @@ let tiempo =0;
 let intervalo = 0;
 let veificador = false;
 
+init();
+
 function init(){
     iniciar.addEventListener('click',iniciarContador);
     resetear.addEventListener('click',resetearContador);
@@ -17,5 +19,39 @@ function init(){
 }
 
 function iniciarContador(){
+    if(!verificar){
+        var intervalo = setInterval(function (){
+            tiempo += 0.01;
+            temporizador.innerHTML = tiempo.toFixed(2);
+        }, 10)
+        verificar = true;
+    }else{
+        verificar = false;
+        clearInterval(intervalo)
+    }
+}
 
+function resetearContador(){
+    verificar = false;
+    tiempo = 0;
+    temporizador.innerHTML = tiempo + '.00';
+    clearInterval(intervalo);
+    while(almacenarTiempos.firstChild){
+        almacenarTiempos.removeChild(almacenarTiempos.firstChild);
+    }
+}
+
+function grabarContador(){
+    if(temporizador.textContent === '0.00'){
+        console.log('click para iniciar el cronometro');
+    }else{
+        let p = document.createElement('ul');
+        p.innerHTML = `
+        
+        <li>Tiempo: ${tiempo.toFixed(2)}</li>
+
+        `;
+
+        almacenarTiempos.appendChild(p);
+    }
 }
